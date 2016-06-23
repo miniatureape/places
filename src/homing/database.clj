29db2d262ff6056d7ab28ad1b7c4f57489a5463e
jdonato <jdonato@etsy.com>
2016-06-23 14:15:09 -0400

    cru events

--- src/homing/database.clj
+++ src/homing/database.clj
@@ -7,7 +7,7 @@
 
 (defn create-places-table []
   (jdbc/create-table-ddl :places
-                         [[:longtitude :text]
+                         [[:longitude :text]
                           [:latitude :text]
                           [:title :text]
                           [:description :text]]))
@@ -15,3 +15,20 @@
 (defn create-db []
   (try (jdbc/db-do-commands db (create-places-table))
   (catch Exception e (println e))))
+
+(defn query-place
+  "Return place data from longitude and latitude"
+  [longitude latitude]
+  (jdbc/query db ["SELECT * FROM places WHERE longitude = ? AND latitude = ?" longitude latitude]))
+
+(defn insert-place
+  "Add a place"
+  [place-data]
+  (jdbc/insert! db :places place-data))
+
+(defn update-place
+   "Update a place defined by longitude and latitude with place-data"
+   [place-data longitude latitude]
+   (jdbc/update! db :places
+                place-data
+                ["longitude = ? and latitude = ?" longitude latitude]))
