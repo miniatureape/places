ea8e4e3006ad7bf99f0a59903ad49418823d844f
jdonato <jdonato@etsy.com>
2016-06-24 14:47:40 -0400

    Add main function that runs jetty handler

--- src/homing/core.clj
+++ src/homing/core.clj
@@ -1,6 +1,10 @@
-(ns homing.core)
+(ns homing.core
+  (:use ring.adapter.jetty))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
+
+(defn -main []
+  (run-jetty handler {:port 8080}))
