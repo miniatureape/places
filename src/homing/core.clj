66859633d98eccb0a8f141056880ed7aef220ec5
jdonato <jdonato@etsy.com>
2016-06-25 15:00:06 -0400

    adds query and add api endpoint

--- src/homing/core.clj
+++ src/homing/core.clj
@@ -1,10 +1,14 @@
 (ns homing.core
-  (:use ring.adapter.jetty))
+  (:use ring.adapter.jetty)
+  (:require [compojure.core :refer :all])
+  (:require [homing.database :refer [query-place add-place]]))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
 
-(defn -main []
-  (run-jetty handler {:port 8080}))
+(defn get-place [longitude latitude] (query-place longitude latitude))
+(defn add-place [longitude latitude params] (add-place (assoc params :longitude longitude :latitude latitude)))
+
+(defroutes api-routes
+  (GET "/place/:longitude/:latitude" [longitude latitude] (get-place longitude latitude))
+  (POST "/place/:longitude/:latitude" [longitude latitude & params] (add-place longitude latitude params)))
+
+(def application (routes api-routes))
