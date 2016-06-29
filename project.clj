14f125054d12855ceac04f2b159687977c1f1c65
jdonato <jdonato@etsy.com>
2016-06-29 13:31:55 -0400

    add ring json

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [compojure "1.5.1"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.xerial/sqlite-jdbc "3.8.11.2"]
-                 [com.google.maps/google-maps-services "0.1.15"]]
+                 [com.google.maps/google-maps-services "0.1.15"]
+                 [ring/ring-json "0.4.0"]
+                 ]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler homing.core/application :auto-reload? true})
 
